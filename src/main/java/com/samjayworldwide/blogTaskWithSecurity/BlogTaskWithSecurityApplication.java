709d747787ca136abcd23a05a07b482226396fc9
samjayworldwide <samuel.mbanisi@decagon.dev>
2023-11-07 13:54:57 +0100

    testing cicd

---
+++ src/main/java/com/samjayworldwide/blogTaskWithSecurity/BlogTaskWithSecurityApplication.java
@@ -0,0 +1,19 @@
+package com.samjayworldwide.blogTaskWithSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTaskWithSecurityApplication {
+//	echo "# security2" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/Samjayworldwide/security2.git
+//	git push -u origin main
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTaskWithSecurityApplication.class, args);
+	}
+
+}
