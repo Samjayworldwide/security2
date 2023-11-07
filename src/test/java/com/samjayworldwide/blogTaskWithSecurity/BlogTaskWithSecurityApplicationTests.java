19e37db608d39617ec66b439df5b4cb7eb8b83b8
samjayworldwide <samuel.mbanisi@decagon.dev>
2023-11-07 20:02:12 +0100

    new BlogTaskWithSecurityApplicationTests

--- src/test/java/com/samjayworldwide/blogTaskWithSecurity/BlogTaskWithSecurityApplicationTests.java
+++ src/test/java/com/samjayworldwide/blogTaskWithSecurity/BlogTaskWithSecurityApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-integration.properties")
-@ActiveProfiles("integration-tests")
 class BlogTaskWithSecurityApplicationTests {
 
 	@Test
